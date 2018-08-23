27d7b9418a979843e4ebb62a257c498ddb71ced6
Sergey Matsitsky <matsitskys@digicode.net>
2018-08-23 09:48:20 +0300

    start

--- src/components/DetailValue.vue
+++ src/components/DetailValue.vue
@@ -4,8 +4,11 @@
       v-if="field.type === 'number' || field.type === 'string'" 
       class="detailvalue__input" 
       contenteditable="true" 
+      v-html="content()"
+      @focus="onFocus"
+      @blur="onBlur"
       @keydown="keydown" 
-      @input="input">{{ field.value }}</span>
+      @input="input"></span>
     <label 
       v-if="field.type === 'boolean'"
       class="detailvalue__label">
@@ -22,7 +25,31 @@ export default {
   props: {
     field: { type: Object, required: true }
   },
+  data: {
+    isEdit: false,
+    value: ""
+  },
   methods: {
+    content() {
+      if (this.isEdit) {
+        if (this.value === undefined) {
+          this.value = this.field.value;
+        }
+        return this.value;
+      } else {
+        return this.field.value;
+      }
+    },
+    onFocus() {
+      this.isEdit = true;
+      if (this.isEdit && this.value === undefined) {
+        this.value = this.field.value;
+      }
+    },
+    onBlur() {
+      this.isEdit = false;
+      this.setValue(this.value);
+    },
     type() {
       const type = this.field.type;
       if (type === "object" && this.field.value === null) {
@@ -35,16 +62,11 @@ export default {
     },
     input(e) {
       const value = e.target.innerText;
-      if (value.match(/[0-9.]+/)) {
-        this.$emit("change", parseFloat(value, 10));
-      } else if (
-        ["true", "false", "null"].indexOf(value.toLowerCase()) !== -1
-      ) {
-        this.$emit("change", value.toLowerCase());
-      }
+      this.value = value;
+      this.setValue(value);
     },
     toggle() {
-      this.$emit("change", this.field.value);
+      this.setValue(this.field.value);
     },
     keydown(e) {
       let value = parseFloat(e.target.innerText, 10);
@@ -70,7 +92,21 @@ export default {
       }
       if (update) {
         e.target.innerText = value;
-        this.$emit("change", value);
+        this.setValue(value);
+      }
+    },
+    setValue(value) {
+      if (!this.isEdit) {
+        let newValue;
+        if (value.match(/[0-9.]+/)) {
+          newValue = parseFloat(newValue, 10);
+        } else if (
+          ["true", "false", "null"].indexOf(newValue.toLowerCase()) !== -1
+        ) {
+          newValue = newValue.toLowerCase();
+        }
+        this.$emit("change", newValue);
+        this.value = newValue;
       }
     }
   }
