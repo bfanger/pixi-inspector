527e4a1915ef5c1ad75d93cc3e01282ede75721b
Bob Fanger <bob.fanger@this.nl>
2025-09-27 17:40:54 +0200

    feat: miniDetector now supports Phaser

--- packages/pixi-panel/src/ui-mini/miniDetector.ts
+++ packages/pixi-panel/src/ui-mini/miniDetector.ts
@@ -1,50 +1,86 @@
-import type { Application } from "pixi.js";
+import type { Application, Container } from "pixi.js";
 import type { TreeInit } from "ui-protocol/src/types";
 
+const win = window as any;
 export default function miniDetector() {
-  const win = window as any;
-  let app = win.__PIXI_APP__ as Application | undefined;
-
-  const instructionsNode = {
-    component: "Instructions",
-    children: [],
-    props: {},
-    node: {},
-  } satisfies TreeInit;
-  const injectNode = {
-    component: "PixiInject",
-    children: [],
-    props: {},
-    node: {},
-  } satisfies TreeInit;
+  let previous = detectClient();
   let injected = false;
-
-  function createNode(app: Application | undefined): TreeInit {
-    if (!app) {
-      return instructionsNode;
-    }
-    const legacyInit: unknown = win.__PIXI_DEVTOOLS_LEGACY__;
-    if (typeof legacyInit === "function") {
-      injected = true;
-      return legacyInit() as TreeInit;
-    }
-    return injectNode;
-  }
 
   return {
     component: "Fragment",
     props: {},
-    children: [createNode(app)],
+    children: [createNode(previous)],
     node: {
       sync(out) {
-        const detected = win.__PIXI_APP__ as Application | undefined;
-        if (detected && win.__PIXI_DEVTOOLS_LEGACY__ && !injected) {
-          out.replacements.push({ index: 0, ...createNode(detected) });
-        } else if (app !== detected) {
-          out.replacements.push({ index: 0, ...createNode(detected) });
+        const client = detectClient();
+        if (client && win.__PIXI_DEVTOOLS_LEGACY__ && !injected) {
+          injected = true;
+          out.replacements.push({ index: 0, ...createNode(client) });
+        } else if (previous !== client) {
+          out.replacements.push({ index: 0, ...createNode(client) });
         }
-        app = detected;
+        previous = client;
       },
     },
   } satisfies TreeInit;
+}
+
+function createNode(app: DetectedClient): TreeInit {
+  if (!app) {
+    return {
+      component: "Instructions",
+      children: [],
+      props: {},
+      node: {},
+    };
+  }
+  const legacyInit: unknown = win.__PIXI_DEVTOOLS_LEGACY__;
+  if (typeof legacyInit === "function") {
+    return legacyInit() as TreeInit;
+  }
+  return {
+    component: "PixiInject",
+    children: [],
+    props: {},
+    node: {},
+  };
+}
+
+type DetectedClient = ReturnType<typeof detectClient>;
+function detectClient() {
+  const app = getGlobal<Application>("__PIXI_APP__");
+  if (app) {
+    return app;
+  }
+  const stage = getGlobal<Container>("__PIXI_STAGE__");
+  if (stage) {
+    return stage;
+  }
+  const game = getGlobal<unknown>("__PHASER_GAME__");
+  if (game) {
+    return game;
+  }
+  const renderer = getGlobal<unknown>("__PIXI_RENDERER__");
+  if (renderer) {
+    return renderer;
+  }
+  return undefined;
+}
+
+function getGlobal<T>(varname: string): T | undefined {
+  if (varname in win) {
+    return win[varname] as T;
+  }
+  if (win.frames) {
+    for (let i = 0; i < win.frames.length; i += 1) {
+      try {
+        if (varname in win.frames[i]) {
+          return win.frames[i][varname] as T;
+        }
+      } catch {
+        // access to iframe was denied
+      }
+    }
+  }
+  return undefined;
 }
