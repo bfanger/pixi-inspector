f6baa9f82be9355d7109755363ae1009dbaaa256
Bob Fanger <bfanger@gmail.com>
2026-06-06 14:56:49 +0200

    feat: Refactored gizmo-move by hand

--- packages/blender-elements/src/gizmos/gizmo-move.ts
+++ packages/blender-elements/src/gizmos/gizmo-move.ts
@@ -1,104 +1,121 @@
 import { html } from "../html";
 
-const stylesheet = html`
-  <style>
-    .gizmo-move {
-      position: absolute;
+export default class GizmoMoveElement extends HTMLElement {
+  #shadow: ShadowRoot;
+  #arrowX: HTMLElement;
+  #arrowY: HTMLElement;
+  #ring: HTMLElement;
+  #dragging = false;
+
+  constructor() {
+    super();
+
+    this.#arrowX = createArrow("#ff3752", 0);
+    this.#arrowY = createArrow("#7fcc1c", -90);
+    this.#ring = html`<div class="ring"></div>`;
+
+    this.#shadow = this.attachShadow({ mode: "open" });
+    this.#shadow.append(
+      createStylesheet(),
+      this.#ring,
+      this.#arrowX,
+      this.#arrowY,
+    );
+
+    this.#arrowX.addEventListener("mousedown", (e) => this.#dragStart(e, "x"));
+    this.#arrowY.addEventListener("mousedown", (e) => this.#dragStart(e, "y"));
+    this.#ring.addEventListener("mousedown", (e) => this.#dragStart(e));
+  }
+
+  connectedCallback() {
+    if (!this.style.position) {
+      this.style.position = "absolute";
+    }
+  }
+
+  getPosition() {
+    return {
+      x: parseInt(this.style.left) || 0,
+      y: parseInt(this.style.top) || 0,
+    };
+  }
+
+  setPosition(x: number, y: number) {
+    if (this.#dragging) {
+      return;
+    }
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
+  }
+
+  #dragStart(event: MouseEvent, axis?: "x" | "y") {
+    event.preventDefault();
+    const start = {
+      ...this.getPosition(),
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+    let x = start.x;
+    let y = start.y;
+    this.#dragging = true;
+    const ghost = createGhost(axis);
+    this.#shadow.append(ghost);
+
+    if (axis === "x") {
+      this.#arrowX.classList.add("dragging");
+      this.#ring.classList.add("hidden");
+      this.#arrowY.classList.add("hidden");
+    } else if (axis === "y") {
+      this.#arrowY.classList.add("dragging");
+      this.#ring.classList.add("hidden");
+      this.#arrowX.classList.add("hidden");
+    } else {
+      this.#ring.classList.add("dragging");
+      this.#arrowX.classList.add("hidden");
+      this.#arrowY.classList.add("hidden");
     }
 
-    .ring {
-      position: absolute;
-      top: -14px;
-      left: -14px;
-      box-sizing: border-box;
-      width: 29px;
-      height: 29px;
-      border: 2px solid currentcolor;
-      border-radius: 50%;
-      color: #fff;
-      opacity: 0.75;
-    }
+    const drag = (e: MouseEvent) => {
+      let deltaX = e.clientX - start.clientX;
+      let deltaY = e.clientY - start.clientY;
+      x = start.x + deltaX;
+      y = start.y + deltaY;
+      if (axis === "x") {
+        y = start.y;
+        deltaY = 0;
+      } else if (axis === "y") {
+        x = start.x;
+        deltaX = 0;
+      }
+      this.style.left = `${x}px`;
+      this.style.top = `${y}px`;
+      ghost.style.left = `${-deltaX}px`;
+      ghost.style.top = `${-deltaY}px`;
 
-    .ring:hover {
-      opacity: 1;
-    }
+      this.dispatchEvent(new CustomEvent("gizmo-drag", { detail: { x, y } }));
+    };
 
-    .ghost .ring {
-      color: #808080bf;
-    }
-
-    .ghost {
-      pointer-events: none;
-      position: absolute;
-    }
-
-    .arrow {
-      position: absolute;
-      left: 0;
-      transform-origin: 0.5px 0.5px;
-      width: 100px;
-      height: 1px;
-      opacity: 0.75;
-    }
-
-    .arrow:hover,
-    .arrow.dragging {
-      opacity: 1;
-    }
-
-    .arrow:not(.dragging) .area {
-      content: "";
-      position: absolute;
-      top: -13px;
-      left: 15px;
-      width: 84px;
-      height: 27px;
-      border-top-right-radius: 13px;
-      border-bottom-right-radius: 13px;
-    }
-
-    .line {
-      content: "";
-      position: absolute;
-      top: -1px;
-      right: 24px;
-      width: 61px;
-      height: 3px;
-      background: linear-gradient(
-        to bottom,
-        transparent 0%,
-        currentcolor 40%,
-        currentcolor 60%,
-        transparent 100%
+    const dragEnd = () => {
+      this.#dragging = false;
+      window.removeEventListener("mousemove", drag);
+      window.removeEventListener("mouseup", dragEnd);
+      ghost.remove();
+      this.#ring.classList.remove("hidden", "dragging");
+      this.#arrowY.classList.remove("hidden", "dragging");
+      this.#arrowX.classList.remove("hidden", "dragging");
+      this.dispatchEvent(
+        new CustomEvent("gizmo-dragend", { detail: { x, y } }),
       );
-    }
+    };
 
-    .arrow.dragging .line {
-      width: 72px;
-    }
+    window.addEventListener("mousemove", drag);
+    window.addEventListener("mouseup", dragEnd);
 
-    .triangle {
-      content: "";
-      position: absolute;
-      top: -4px;
-      right: 6px;
-      border-top: 5px solid transparent;
-      border-bottom: 5px solid transparent;
-      border-left: 18px solid currentcolor;
-    }
-
-    .arrow.dragging .dot {
-      content: "";
-      position: absolute;
-      top: -3px;
-      left: -3px;
-      width: 7px;
-      height: 7px;
-      border-radius: 50%;
-      background: currentcolor;
-    }
-  </style>
-`;
+    this.dispatchEvent(
+      new CustomEvent("gizmo-dragstart", { detail: { x, y } }),
+    );
+  }
+}
 
 function createArrow(color: string, rotate: number): HTMLElement {
   return html`<div
@@ -111,10 +128,6 @@ function createArrow(color: string, rotate: number): HTMLElement {
     <div class="triangle"></div>
   </div>`;
 }
-
-const xArrow = createArrow("#ff3752", 0);
-const yArrow = createArrow("#7fcc1c", -90);
-const ring = html`<div class="ring"></div>`;
 
 function createGhost(axis: "x" | "y" | undefined): HTMLElement {
   const ghost = html`<div class="ghost"></div>`;
@@ -134,134 +147,107 @@ function createGhost(axis: "x" | "y" | undefined): HTMLElement {
   return ghost;
 }
 
-export default class GizmoMoveElement extends HTMLElement {
-  #shadow: ShadowRoot;
-  #gizmo: HTMLElement;
-  #ghost: HTMLElement | null = null;
-  #axis: "x" | "y" | undefined;
-  #dragging = false;
-  #initial = { x: 0, y: 0 };
-
-  #x = 0;
-  #y = 0;
-
-  static observedAttributes = ["x", "y"];
-
-  constructor() {
-    super();
-    this.#shadow = this.attachShadow({ mode: "open" });
-    this.#shadow.append(stylesheet);
-
-    this.#gizmo = html`<div class="gizmo-move"></div>`;
-    this.#shadow.append(this.#gizmo);
-
-    const xArrowClone = xArrow.cloneNode(true) as HTMLElement;
-    const yArrowClone = yArrow.cloneNode(true) as HTMLElement;
-    const ringClone = ring.cloneNode(true) as HTMLElement;
-
-    xArrowClone.addEventListener("mousedown", (e) => this.#dragStart(e, "x"));
-    yArrowClone.addEventListener("mousedown", (e) => this.#dragStart(e, "y"));
-    ringClone.addEventListener("mousedown", (e) => this.#dragStart(e));
-
-    this.#gizmo.append(xArrowClone, yArrowClone, ringClone);
-  }
-
-  connectedCallback() {
-    this.#update();
-  }
-
-  attributeChangedCallback(_name: string, _old: string, newVal: string) {
-    if (_name === "x") {
-      this.#x = parseFloat(newVal) || 0;
-    } else if (_name === "y") {
-      this.#y = parseFloat(newVal) || 0;
-    }
-    this.#update();
-  }
-
-  set x(value: number) {
-    this.#x = value;
-    this.#update();
-  }
-
-  get y(): number {
-    return this.#y;
-  }
-  set y(value: number) {
-    this.#y = value;
-    this.#update();
-  }
-
-  #update() {
-    this.#gizmo.style.left = `${this.#x}px`;
-    this.#gizmo.style.top = `${this.#y}px`;
-
-    const ringEl = this.#gizmo.querySelector<HTMLElement>(".ring");
-    if (ringEl) {
-      ringEl.style.display =
-        this.#dragging && this.#axis !== undefined ? "none" : "";
-    }
-
-    if (this.#ghost) {
-      this.#ghost.style.left = `${this.#initial.x}px`;
-      this.#ghost.style.top = `${this.#initial.y}px`;
-    }
-
-    const arrows = this.#gizmo.querySelectorAll(".arrow");
-    arrows.forEach((arrow, i) => {
-      const isX = i === 0;
-      const isY = i === 1;
-      if (this.#dragging && this.#axis === "x" && isX) {
-        arrow.classList.add("dragging");
-      } else if (this.#dragging && this.#axis === "y" && isY) {
-        arrow.classList.add("dragging");
-      } else {
-        arrow.classList.remove("dragging");
+function createStylesheet() {
+  return html`
+    <style>
+      .hidden {
+        display: none;
       }
-    });
-  }
 
-  #dragStart(event: MouseEvent, restrict?: "x" | "y") {
-    event.preventDefault();
-    this.#axis = restrict;
-    this.#initial = { x: this.#x, y: this.#y };
-    this.#dragging = true;
+      .ring {
+        position: absolute;
+        top: -14px;
+        left: -14px;
+        box-sizing: border-box;
+        width: 29px;
+        height: 29px;
+        border: 2px solid currentColor;
+        border-radius: 50%;
+        color: #fff;
+        opacity: 0.75;
+        &:hover {
+          opacity: 1;
+        }
 
-    const startClientX = event.clientX;
-    const startClientY = event.clientY;
-    const initialX = this.#initial.x;
-    const initialY = this.#initial.y;
-
-    const drag = (e: MouseEvent) => {
-      if (restrict === "x") {
-        this.#x = initialX + (e.clientX - startClientX);
-      } else if (restrict === "y") {
-        this.#y = initialY + (e.clientY - startClientY);
-      } else {
-        this.#x = initialX + (e.clientX - startClientX);
-        this.#y = initialY + (e.clientY - startClientY);
+        &:not(.dragging) {
+          filter: drop-shadow(0px 0px 2px #0003);
+        }
       }
-      this.#update();
-    };
 
-    const dragEnd = () => {
-      window.removeEventListener("mousemove", drag);
-      window.removeEventListener("mouseup", dragEnd);
-      this.#axis = undefined;
-      this.#dragging = false;
-      this.#ghost = null;
-      this.#shadow.querySelector(".ghost")?.remove();
-      this.#update();
-    };
+      .ghost .ring {
+        color: #808080bf;
+      }
 
-    window.addEventListener("mousemove", drag);
-    window.addEventListener("mouseup", dragEnd);
+      .ghost {
+        pointer-events: none;
+        position: absolute;
+      }
 
-    this.#ghost = createGhost(this.#axis);
-    this.#ghost.style.left = `${this.#initial.x}px`;
-    this.#ghost.style.top = `${this.#initial.y}px`;
-    this.#shadow.append(this.#ghost);
+      .arrow {
+        position: absolute;
+        left: 0;
+        transform-origin: 0.5px 0.5px;
+        width: 100px;
+        height: 1px;
+        opacity: 0.75;
 
-    this.#update();
-  }
+        &:hover,
+        &.dragging {
+          opacity: 1;
+        }
+
+        &:not(.dragging) .area {
+          content: "";
+          position: absolute;
+          top: -13px;
+          left: 15px;
+          width: 84px;
+          height: 27px;
+          border-top-right-radius: 13px;
+          border-bottom-right-radius: 13px;
+        }
+      }
+
+      .line {
+        content: "";
+        position: absolute;
+        top: -1px;
+        right: 24px;
+        width: 61px;
+        height: 3px;
+        background: linear-gradient(
+          to bottom,
+          transparent 0%,
+          currentColor 40%,
+          currentColor 60%,
+          transparent 100%
+        );
+      }
+
+      .arrow.dragging .line {
+        width: 72px;
+      }
+
+      .triangle {
+        content: "";
+        position: absolute;
+        top: -4px;
+        right: 6px;
+        border-top: 5px solid transparent;
+        border-bottom: 5px solid transparent;
+        border-left: 18px solid currentColor;
+      }
+
+      .arrow.dragging .dot {
+        content: "";
+        position: absolute;
+        top: -3px;
+        left: -3px;
+        width: 7px;
+        height: 7px;
+        border-radius: 50%;
+        background: currentColor;
+      }
+    </style>
+  `;
 }
--- packages/pixi-panel/src/pixi-overlay.ts
+++ packages/pixi-panel/src/pixi-overlay.ts
@@ -3,7 +3,25 @@ import defineElements, {
 } from "../../blender-elements/src/gizmos/defineElements";
 
 defineElements();
+const win = window as any;
+
 const move = document.createElement("gizmo-move") as GizmoMoveElement;
-move.x = 40;
-move.y = 40;
-document.body.appendChild(move);
+move.addEventListener("gizmo-drag", (e) => {
+  const pos = (e as CustomEvent).detail;
+  win.$pixi.position.set(pos.x, pos.y);
+});
+
+let previous: any = undefined;
+function sync() {
+  if (typeof win.$pixi?.position?.x === "number") {
+    move.setPosition(win.$pixi.position.x, win.$pixi.position.y);
+  }
+  if (!win.$pixi && previous) {
+    move.remove();
+  } else if (win.$pixi && !previous) {
+    document.body.appendChild(move);
+  }
+  previous = win.$pixi;
+  requestAnimationFrame(sync);
+}
+sync();
